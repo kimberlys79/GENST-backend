2944a5d708208038821b16e2c664cf71e044afc9
kimberlys79 <ygksianipar@gmail.com>
2025-06-09 17:18:47 +0700

    New Deploy

--- src/config/database.js
+++ src/config/database.js
@@ -3,7 +3,7 @@ const mysql = require('mysql2');
 const dbPool =mysql.createPool({
     host: process.env.DB_HOST,
     user: process.env.DB_USERNAME,
-    port: process.env.PORT,
+    port: process.env.DB_PORT,
     password: process.env.DB_PASSWORD,
     database:process.env.DB_NAME,
     ssl: {
