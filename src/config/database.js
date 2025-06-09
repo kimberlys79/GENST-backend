c99a5d09ad6d8d2b94d7902ce942599907ba5909
kimberlys79 <ygksianipar@gmail.com>
2025-06-09 15:59:17 +0700

    Debuggin Error

--- src/config/database.js
+++ src/config/database.js
@@ -5,6 +5,9 @@ const dbPool =mysql.createPool({
     user: process.env.DB_USERNAME,
     password: process.env.DB_PASSWORD,
     database:process.env.DB_NAME,
+    ssl: {
+        rejectUnauthorized: true
+    }
 });
 
 module.exports = dbPool.promise();
