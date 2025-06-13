14f2229b42f7cf5d17867ba38cae99916235aefb
kimberlys79 <ygksianipar@gmail.com>
2025-06-13 11:03:05 +0700

    Fix Debugging

--- src/config/database.js
+++ src/config/database.js
@@ -3,7 +3,7 @@ const mysql = require('mysql2');
 const dbPool =mysql.createPool({
     host: process.env.DB_HOST,
     user: process.env.DB_USERNAME,
-    port: process.env.DB_PORT,
+    port: parseInt(process.env.DB_PORT, 10),
     password: process.env.DB_PASSWORD,
     database:process.env.DB_NAME,
     ssl: {
