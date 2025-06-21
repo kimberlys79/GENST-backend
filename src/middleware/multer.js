5a7e719b851e1c793d853d0fff011b80b3034fdd
kimberlys79 <ygksianipar@gmail.com>
2025-06-21 12:56:50 +0700

    Adjust Multer Logic

--- src/middleware/multer.js
+++ src/middleware/multer.js
@@ -1,26 +1,47 @@
 const multer = require('multer');
 const path = require('path');
 
-const storage = multer.diskStorage({
-    destination: (req, file, cb) => {
-        if (file.fieldname === 'report_pdf') {
-            cb(null, 'public/uploads/pdf');  // simpan PDF di sini
-        } else {
-            cb(null, 'public/images');       // image tetap di sini
-        }
+// Storage untuk image (photo & sign)
+const imageStorage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, 'public/uploads/images/');
     },
-    filename: (req, file, cb) => {
-        const timestamp = Date.now();
-        const originalname = file.originalname;
-        cb(null, `${timestamp}-${originalname}`);
+    filename: function (req, file, cb) {
+        const uniqueSuffix = Date.now() + '-' + file.originalname;
+        cb(null, uniqueSuffix);
     }
 });
 
-const upload = multer({
-    storage: storage,
-    limits: {
-        fileSize: 15 * 1000 * 1000 //15MB
+// Storage untuk PDF
+const pdfStorage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, 'public/uploads/pdf/');
+    },
+    filename: function (req, file, cb) {
+        const uniqueSuffix = Date.now() + '-' + file.originalname;
+        cb(null, uniqueSuffix);
     }
+});
+
+const fileFilter = (req, file, cb) => {
+    if (file.fieldname === 'upload_photo' || file.fieldname === 'inspector_sign') {
+        cb(null, true);
+    } else if (file.fieldname === 'report_pdf') {
+        cb(null, true);
+    } else {
+        cb(new Error('Unsupported file'), false);
+    }
+};
+
+const upload = multer({
+    storage: function (req, file, cb) {
+        if (file.fieldname === 'report_pdf') {
+            cb(null, pdfStorage);
+        } else {
+            cb(null, imageStorage);
+        }
+    },
+    fileFilter: fileFilter
 });
 
 module.exports =upload;
